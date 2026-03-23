ee2649eabca8512651808c4229559d190ec5f217
fb00s <felipeboos18@gmail.com>
2026-03-23 16:58:56 -0300

    feat: add OrdemInvalidaException

--- Problema02/src/main/java/org/example/dominio/empresa/Empresa.java
+++ Problema02/src/main/java/org/example/dominio/empresa/Empresa.java
@@ -1,8 +1,6 @@
 package org.example.dominio.empresa;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.example.dominio.acao.PrecoAcao;
