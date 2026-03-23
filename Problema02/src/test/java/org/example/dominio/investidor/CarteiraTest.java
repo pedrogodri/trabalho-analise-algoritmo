db54a7b05bcfdf7460a8caa8718d993d57a7b869
Pedro Godri <86849487+pedrogodri@users.noreply.github.com>
2026-03-23 09:07:35 -0300

    Add UML diagrams and update pom/tests

--- Problema02/src/test/java/org/example/dominio/investidor/CarteiraTest.java
+++ Problema02/src/test/java/org/example/dominio/investidor/CarteiraTest.java
@@ -5,7 +5,9 @@ import org.example.dominio.empresa.NomeDaEmpresa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CarteiraTest {
 
